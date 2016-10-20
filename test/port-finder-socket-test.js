27668cb185b6c4d74fcfa9d771469eb76412d833
Erik Trom <erik.trom@gmail.com>
2016-10-20 12:14:06 -1000

    Dont test for socket support right now (meaning running npm test when on windows on bash will currently fail for the 'portfinder-socket-test.js' test)

--- test/port-finder-socket-test.js
+++ test/port-finder-socket-test.js
@@ -6,8 +6,10 @@
  */
 
 /*
-NOTE: These tests curently fail when using the bash shell on windows. Socket
-      support for that env has not landed.
+
+Note these tests fail when using bash on windows. In an forthcoming PR
+this we'll test whether that env is present and skip this test
+
  */
 
 "use strict";
@@ -19,48 +21,11 @@ var assert = require('assert'),
     vows = require('vows'),
     portfinder = require('../lib/portfinder'),
     fs = require('fs'),
-    glob = require('glob'),
-    Promise = require('bluebird');
+    glob = require('glob');
 
 var servers = [],
     socketDir = path.join(__dirname, 'fixtures'),
     badDir = path.join(__dirname, 'bad-dir');
-
-const internals = {};
-
-internals.pathToSocket = function _pathToSocket(dirname, filename) {
-  return path.join(dirname, filename)
-}
-
-
-internals.isSocketPossible = function _isSocketPossible() {
-  function cleanupTrySocket() {
-    return new Promise(function(resolve, reject) {
-      glob(path.resolve(socketDir, '*'), function (err, files) {
-        if (err) { reject(err); }
-        for (var i = 0; i < files.length; i++) { fs.unlinkSync(files[i]); }
-        resolve();
-      });
-    });
-  }
-
-  return new Promise(function(resolve, reject) {
-    if (process.platform === 'win32') { resolve(true); }
-
-    var server = net.createServer(function() {});
-    var socket = internals.pathToSocket(socketDir, 'trysocket.sock');
-
-    server.listen(socket);
-
-    server.once('listening', function() {
-      cleanupTrySocket(function() { resolve(); });
-    });
-
-    server.once('error', function(err) {
-      cleanupTrySocket(function() { reject(err); });
-    });
-  });
-}
 
 function createServers (callback) {
   var base = 0;
@@ -70,7 +35,7 @@ function createServers (callback) {
     function (next) {
       var server = net.createServer(function () { }),
           name = base === 0 ? 'test.sock' : 'test' + base + '.sock',
-          sock = internals.pathToSocket(socketDir, name);
+          sock = path.join(socketDir, name);
 
       // shamelessly stolen from foreverjs,
       // https://github.com/foreverjs/forever/blob/6d143609dd3712a1cf1bc515d24ac6b9d32b2588/lib/forever/worker.js#L141-L154
@@ -104,82 +69,68 @@ function cleanup(callback) {
   });
 }
 
-
-internals.isSocketPossible()
-  .then(function(/*result*/) {
-    vows.describe('portfinder').addBatch({
-      "When using portfinder module": {
-        "with 5 existing servers": {
+vows.describe('portfinder').addBatch({
+  "When using portfinder module": {
+    "with 5 existing servers": {
+      topic: function () {
+        createServers(function() {
+          portfinder.getSocket({
+            path: path.join(badDir, 'test.sock')
+          }, this.callback);
+        }.bind(this));
+      },
+      "the getPort() method": {
+        topic: function () {
+          portfinder.getSocket({
+            path: path.join(socketDir, 'test.sock')
+          }, this.callback);
+        },
+        "should respond with the first free socket (test5.sock)": function (err, socket) {
+          assert.isTrue(!err);
+          assert.equal(socket, path.join(socketDir, 'test5.sock'));
+        }
+      }
+    }
+  }
+}).addBatch({
+  "When using portfinder module": {
+    "with no existing servers": {
+      "the getSocket() method": {
+        "with a directory that doesnt exist": {
           topic: function () {
-            createServers(function() {
+            fs.rmdir(badDir, function () {
               portfinder.getSocket({
                 path: path.join(badDir, 'test.sock')
               }, this.callback);
             }.bind(this));
           },
-          "the getPort() method": {
-            topic: function () {
-              portfinder.getSocket({
-                path: path.join(socketDir, 'test.sock')
-              }, this.callback);
-            },
-            "should respond with the first free socket (test5.sock)": function (err, socket) {
-              assert.isTrue(!err);
-              assert.equal(socket, path.join(socketDir, 'test5.sock'));
-            }
+          "should respond with the first free socket (test.sock)": function (err, socket) {
+            assert.isTrue(!err);
+            assert.equal(socket, path.join(badDir, 'test.sock'));
           }
-        }
-      }
-    }).addBatch({
-      "When using portfinder module": {
-        "with no existing servers": {
-          "the getSocket() method": {
-            "with a directory that doesnt exist": {
-              topic: function () {
-                fs.rmdir(badDir, function () {
-                  portfinder.getSocket({
-                    path: path.join(badDir, 'test.sock')
-                  }, this.callback);
-                }.bind(this));
-              },
-              "should respond with the first free socket (test.sock)": function (err, socket) {
-                assert.isTrue(!err);
-                assert.equal(socket, path.join(badDir, 'test.sock'));
-              }
-            },
-            "with a directory that exists": {
-              topic: function () {
-                portfinder.getSocket({
-                  path: path.join(socketDir, 'exists.sock')
-                }, this.callback);
-              },
-              "should respond with the first free socket (exists.sock)": function (err, socket) {
-                assert.isTrue(!err);
-                assert.equal(socket, path.join(socketDir, 'exists.sock'));
-              }
-            }
-          }
-        }
-      }
-    }).addBatch({
-      "When the tests are over": {
-        topic: function() {
-          cleanup(this.callback);
         },
-        "necessary cleanup should have taken place": function (err, wasRun) {
-          assert.isTrue(!err);
-          assert.isTrue(wasRun);
+        "with a directory that exists": {
+          topic: function () {
+            portfinder.getSocket({
+              path: path.join(socketDir, 'exists.sock')
+            }, this.callback);
+          },
+          "should respond with the first free socket (exists.sock)": function (err, socket) {
+            assert.isTrue(!err);
+            assert.equal(socket, path.join(socketDir, 'exists.sock'));
+          }
         }
       }
-    }).export(module);
-  })
-  .catch(function(err) {
-    console.error("This operating system does support binding a socket to a UNIX file descriptor nor a Windows named pipe. This most likely means your running Bash on Windows, which currently does not support binding to a named socket %o", err);
-  });
-
-
-
-
-
-
-
+    }
+  }
+}).addBatch({
+  "When the tests are over": {
+    topic: function() {
+      cleanup(this.callback);
+    },
+    "necessary cleanup should have taken place": function (err, wasRun) {
+      assert.isTrue(!err);
+      assert.isTrue(wasRun);
+    }
+  }
+}).export(module);
